f5f62c910af13d5f60ea321b77b2b58432b65bc7
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-08-27 18:22:37 -0700

    Create test suite with unit test cases.

--- specs/client/spec.js
+++ specs/client/spec.js
@@ -1,8 +1,59 @@
 var expect = require('chai').expect;
 
-describe('Basic test', function() {
-  it('should return text', function() {
-    var something = 'This is something.';
-    expect(something).to.equal('This is something.');
+describe('Loading a board', function() {
+  xit('should load the selected board into the game board', function() {
+
+  });
+
+  xit('should load the corresponding solved board into the solution board', function() {
+
+  });
+
+  xit('should update the DOM nodes with the appropriate board values', function() {
+
+  });
+});
+
+describe('Manipulating a board', function() {
+  xit('should change a value in a mutable cell', function() {
+
+  });
+
+  xit('should update the DOM node with the appropriate cell value', function() {
+
+  });
+
+  xit('should not change a value in an immutable cell', function() {
+
+  });
+});
+
+describe('Checking board conflicts', function() {
+  xit('should detect all board conflicts for a given conflicting value', function() {
+
+  });
+
+  xit('should remove all relevant conflicts when a conflicting value is changed', function() {
+
+  });
+});
+
+describe('Checking board solved state', function() {
+  xit('should trigger the win state on a solved board', function() {
+
+  });
+});
+
+describe('Generating a new board', function() {
+  xit('should generate a new easy difficulty board', function() {
+
+  });
+
+  xit('should generate a new medium difficulty board', function() {
+
+  });
+
+  xit('should generate a new hard difficulty board', function() {
+
   });
 });
